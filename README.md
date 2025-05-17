126a289c0f8d6773f3a7ff720447751b42571c4d
Farbfetzen <farbfetzen@posteo.de>
2025-05-17 15:28:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring-Boot-PostgreSQL
+
+A project to learn Docker and PostgreSQL with Spring Boot.
---
+++ src/test/java/farbfetzen/spring_boot_postgresql/ApplicationTests.java
@@ -0,0 +1,12 @@
+package farbfetzen.spring_boot_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
